b582e1459736a35f02215c259a918bd0598d1e11
Iulia Sandu <isandu@griddynamics.com>
2025-08-29 18:31:06 +0300

    Update delete test and test names

--- src/test/java/org/example/RA/boards/DeleteBoardTest.java
+++ src/test/java/org/example/RA/boards/DeleteBoardTest.java
@@ -4,21 +4,31 @@ import io.restassured.response.Response;
 import org.example.RA.client.Endpoints;
 import org.example.RA.models.Board;
 import org.example.RA.support.BaseTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class DeleteBoardTest extends BaseTest {
 
+    private String boardId;
+    protected String initialName() {    return "TemporaryBoard";    }
+    protected String initialDesc() {    return "Created for test";   }
+
+    @BeforeMethod
+    public void createdBoard() {
+        Response create = send.post(
+                Endpoints.BOARDS,
+                build.createBoardParams(initialName(), initialDesc(), false)
+        );
+        boardId = prepare.asPojo(create, Board.class).getId();
+    }
+
     @Test
-    public void deleteBoard() {
+    public void shouldDeleteBoardSuccessfully() {
 
-        Response resp = send.post(Endpoints.BOARDS,
-                build.createBoardParams("TmpDelete", "for DELETE", false));
-        String id = prepare.asPojo(resp, Board.class).getId();
-
-        Response del =  send.delete(Endpoints.BOARDS_BY_ID, build.boardIdPathParams(id));
+        Response del =  send.delete(Endpoints.BOARDS_BY_ID, build.boardIdPathParams(boardId));
         check.statusIs(del, 200);
 
-        Response getAfter = send.get(Endpoints.BOARDS_BY_ID, build.boardIdPathParams(id));
+        Response getAfter = send.get(Endpoints.BOARDS_BY_ID, build.boardIdPathParams(boardId));
         check.statusIs(getAfter, 404);
     }
 }
